f90014ffec496e8f927d68313ec84eb82109c16b
徐庆臣 <19655910@qq.com>
2018-08-02 16:35:25 +0800

    新增initview方法

--- app/src/main/java/com/example/kson/git1603b/MainActivity.java
+++ app/src/main/java/com/example/kson/git1603b/MainActivity.java
@@ -9,5 +9,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    /**
+     * 初始化控件
+     */
+    private void initView() {
     }
 }
